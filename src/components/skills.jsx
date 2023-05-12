5641a87134d188f4334d137b96bb023f29f21568
Calvin Lee <Anthony2006Lee@hotmail.com>
2023-05-12 06:33:36 -0500

    fixed my images in skills.jsx

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -25,53 +25,39 @@ const skills = () => {
                         <p className='my-4'>HTML</p>
                     </div>
 
-                    <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-                        <div className='shadow-md shadow-[#040c161] hover:scale-110 duration-500'>
-                            <img className='w-20 mx-auto' src={CSS} alt="CSS logo" />
-                            <p className='my-4'>CSS</p>
-                        </div>
+                    <div className='shadow-md shadow-[#040c161] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={CSS} alt="CSS logo" />
+                        <p className='my-4'>CSS</p>
                     </div>
 
-                    <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-                        <div className='shadow-md shadow-[#040c161] hover:scale-110 duration-500'>
-                            <img className='w-20 mx-auto' src={JavaScript} alt="JavaScript logo" />
-                            <p className='my-4'>JAVASCRIPT</p>
-                        </div>
+                    <div className='shadow-md shadow-[#040c161] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={JavaScript} alt="JavaScript logo" />
+                        <p className='my-4'>JAVASCRIPT</p>
                     </div>
 
-                    <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-                        <div className='shadow-md shadow-[#040c161] hover:scale-110 duration-500'>
-                            <img className='w-20 mx-auto' src={ReactImage} alt="React logo" />
-                            <p className='my-4'>REACT</p>
-                        </div>
+                    <div className='shadow-md shadow-[#040c161] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={ReactImage} alt="React logo" />
+                        <p className='my-4'>REACT</p>
                     </div>
 
-                    <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-                        <div className='shadow-md shadow-[#040c161] hover:scale-110 duration-500'>
-                            <img className='w-20 mx-auto' src={Bootstrap} alt="Bootstrap logo" />
-                            <p className='my-4'>BOOTSTRAP</p>
-                        </div>
+                    <div className='shadow-md shadow-[#040c161] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={Bootstrap} alt="Bootstrap logo" />
+                        <p className='my-4'>BOOTSTRAP</p>
                     </div>
 
-                    <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-                        <div className='shadow-md shadow-[#040c161] hover:scale-110 duration-500'>
-                            <img className='w-20 mx-auto' src={Tailwind} alt="Tailwind logo" />
-                            <p className='my-4'>TAILWIND</p>
-                        </div>
+                    <div className='shadow-md shadow-[#040c161] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={Tailwind} alt="Tailwind logo" />
+                        <p className='my-4'>TAILWIND</p>
                     </div>
 
-                    <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-                        <div className='shadow-md shadow-[#040c161] hover:scale-110 duration-500'>
-                            <img className='w-20 mx-auto' src={Node} alt="Node logo" />
-                            <p className='my-4'>NODE</p>
-                        </div>
+                    <div className='shadow-md shadow-[#040c161] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={Node} alt="Node logo" />
+                        <p className='my-4'>NODE</p>
                     </div>
 
-                    <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-                        <div className='shadow-md shadow-[#040c161] hover:scale-110 duration-500'>
-                            <img className='w-20 mx-auto' src={Github} alt="Github logo" />
-                            <p className='my-4'>GITHUB</p>
-                        </div>
+                    <div className='shadow-md shadow-[#040c161] hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={Github} alt="Github logo" />
+                        <p className='my-4'>GITHUB</p>
                     </div>
                 </div>
             </div>
