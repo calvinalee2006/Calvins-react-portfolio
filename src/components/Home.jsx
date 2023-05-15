86ae4a746edd24488d1249661e4a40aba976e8fd
Calvin Lee <Anthony2006Lee@hotmail.com>
2023-05-15 07:13:29 -0500

    added my home.jsx intro paragraph.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,13 @@ const Home = () => {
                 <p className="text-pink-600 sm:text-7xl font-bold text-[#ccd6f6]">Hello, my name is</p>
                 <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Calvin Lee</h1>
                 <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]"> I am a Javascript software developer </h2>
-                <p className="text-[#8892b0] py-4 max-w-[700]">Self introduction</p>
+                <p className="text-[#8892b0] py-4 max-w-[700]">
+                    A self-taught developer that has used coding languages such as HTML, CSS, JavaScript with React,
+                    Bootstrap, and Node frameworks to build projects and applications. Quick to adapt to changes in the
+                    technological community and will work well as an individual or with teams to produce
+                    working applications. A detailed-oriented developer with a background as a mechanical
+                    technician, that is accustomed to working excessively to achieve the optimal result application
+                    for the customer.</p>
                 <div>
                     <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
                         View Work
