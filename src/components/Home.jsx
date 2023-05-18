3b57db758ae02379356f4ca43dd7139a33d4be58
Calvin Lee <Anthony2006Lee@hotmail.com>
2023-05-18 17:26:38 -0500

    trying to add a PDF resume to project

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <div name="home" className='w-full h-screen bg-[#0a192f] '>
             {/*Container */}
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-                <p className="text-pink-600 sm:text-7xl font-bold text-[#ccd6f6]">Hello, my name is</p>
+                <p className="text-pink-600">Hello, my name is</p>
                 <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Calvin Lee</h1>
                 <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]"> I am a software developer </h2>
                 <p className="text-[#8892b0] py-4 max-w-[700]">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react'
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
-import { Link } from 'react-scroll'
+import { resume } from '../assets/resume.pdf'
+import { Link } from 'react-scroll';
 
 
 const Navbar = () => {
@@ -14,33 +15,32 @@ const Navbar = () => {
             <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
 
                 {/* menu */}
-                <ul className=" hidden md:flex flex-row">
-                    <li className='py-6 text-4xl'>
+                <ul className=" hidden md:flex flex-row float-right">
+                    <li>
                         <Link to="home" smooth={true} duration={500}>
                             Home
                         </Link>
                     </li>
-                    <li className='py-6 text-4xl'>
+                    <li>
                         <Link to="about" smooth={true} duration={500}>
                             About
                         </Link>
                     </li>
-                    <li className='py-6 text-4xl'>
+                    <li>
                         <Link to="skills" smooth={true} duration={500}>
                             Skills
                         </Link>
                     </li>
-                    <li className='py-6 text-4xl'>
+                    <li>
                         <Link to="work" smooth={true} duration={500}>
                             Work
                         </Link>
                     </li>
-                    <li className='py-6 text-4xl'>
+                    <li>
                         <Link to="contact" smooth={true} duration={500}>
                             Contact
                         </Link>
                     </li>
-                    <li className='py-6 text-4xl'></li>
                 </ul>
 
 
@@ -113,7 +113,7 @@ const Navbar = () => {
                         <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
                             <a
                                 className="flex justify-between items-center w-full text-gray-300"
-                                href="/"
+                                href="file:///C:/Users/Antho/OneDrive/Desktop/mastermind%20Course%202/web%20developer%20resume%202023.pdf"
                             >
                                 Resume <BsFillPersonLinesFill size={30} />
                             </a>
