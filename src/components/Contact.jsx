5b34d9b34a7a24d6723f37a8816c0c9b531d9f62
Calvin Lee <Anthony2006Lee@hotmail.com>
2023-05-23 06:45:40 -0500

    added anchor tags on contacts.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import Resume from '../assets/resume.pdf'
 
 function Contact() {
     return (
@@ -7,7 +11,51 @@ function Contact() {
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
                     <p className='text-gray-300 py-4'>// Submit the form below or email me at Calvinalee2006@hotmail.com</p>
-                    <p className='text-greay-300 py-4'></p>
+                    {/* container */}
+                    <div className="display: block">
+
+                        {/* First anchor */}
+                        <div>
+                            <a
+                                className="flex justify-between items-center w-full text-gray-300"
+                                href="https://www.linkedin.com/in/calvinalee/"
+                            >
+                                <FaLinkedin size={40} />
+                            </a>
+                        </div>
+
+                        {/* Second Anchor */}
+                        <div>
+                            <a
+                                className="flex justify-between items-center w-full text-gray-300"
+                                href="https://github.com/calvinalee2006"
+                            >
+                                <FaGithub size={40} />
+                            </a>
+                        </div>
+
+                        {/* Third Anchor */}
+                        <div>
+                            <a
+                                className="flex justify-between items-center w-full text-gray-300"
+                                href="https://www.CalvinALee2006@hotmail.com"
+                            >
+                                <HiOutlineMail size={40} />
+                            </a>
+
+                            {/* Fourth Anchor */}
+                            <div>
+                                <a
+                                    className="flex justify-between items-center w-full text-gray-300"
+                                    href={Resume}
+                                >
+                                    <BsFillPersonLinesFill size={40} />
+                                </a>
+                            </div>
+                        </div>
+
+
+                    </div>
                 </div>
                 <input className='bg-[#ccd6f6] p-2' type="text" placeholder="Name" name="name" />
                 <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder="Email" name="email" />
